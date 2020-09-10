57b21f592fd2d20aec857c56514aaeb6b4fd1c99
nagajayasree <52131854+nagajayasree@users.noreply.github.com>
2020-09-10 21:55:48 +0530

    Update NavBar.jsx

--- Components/NavBar.jsx
+++ Components/NavBar.jsx
@@ -13,17 +13,6 @@ class NavBar extends Component {
           <a class="navbar-brand" href="#">
             Sanskrit Central
           </a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
               <li class="nav-item">
